0cbad7052ea981bc5542e3eae719c692afbdf635
edel08 <edel.william@outlook.com>
2022-09-22 16:54:49 +0700

    Changes
    Signed-off-by: edel08 <edel.william@outlook.com>

--- test.sql
+++ test.sql
@@ -7,4 +7,7 @@ select * from ejejje
 
 if 'i have' to 'leave'ssafff
 
-nothung gonnachange my life for yousssnothung gonnachange my life for youssssss
+nothung gonnachange my life for yousssnothung gonnachange my life for youssssss'nothung gonnachange my life for youssssss' -> Changing  this one into another lyrics
+
+you can use mine 
+'Nothing gonna change my love for you'
