8a5f5995fc93835058a7b5c3c84cfc35de56831f
edel08 <edel.william@outlook.com>
2022-09-29 17:16:56 +0700

    testing from sql server

---
+++ VersionSQL/DESKTOP-GL0DDFA/SQLEXPRESS/Training/dbo/Table/Order.sql
@@ -0,0 +1,11 @@
+﻿/****** Object:  Table [dbo].[Order]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[Order](
+	[Order_No] [float] NULL,
+	[P_Id] [float] NULL,
+	[Order_Date] [datetime] NULL,
+	[Item_Id] [float] NULL,
+	[Qty] [float] NULL
+) ON [PRIMARY]
